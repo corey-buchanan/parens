56dd6cdb2d418dced8ac3a9026f4c9bc8afb7f4e
Corey <coreybtechdog@gmail.com>
2023-04-07 16:38:09 -0600

    First commit - running server!

---
+++ dev/dev.clj
@@ -0,0 +1,16 @@
+(ns dev
+  (:require [integrant.repl :as repl]
+            [undead.system :as system]))
+
+(defn start []
+  (set! *print-namespace-maps* false)
+  (repl/set-prep! (constantly system/system))
+  (repl/go))
+
+(defn stop []
+  (repl/halt))
+
+(comment
+  (start)
+  (stop)
+  )
